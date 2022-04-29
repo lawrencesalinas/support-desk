70411bfc18957cdaf960bf4973a41ffc2348a8c7
Lawrence <lawrencesalinas@yahoo.com>
2022-04-29 16:44:02 -0400

    created error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 4000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.json({ message: 'Hello' })
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
