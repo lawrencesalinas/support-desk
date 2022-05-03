059aefb255feaa385b0a5beee7c15e2b14410896
Lawrence <lawrencesalinas@yahoo.com>
2022-05-02 20:09:17 -0400

    redux setup for user login and sign up page

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
+
+// Register new user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    console.log(user)
+  }
+)
+
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  console.log(user)
+})
+
+// Login user
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  // builder allows to add casse
+  extraReducers: (builder) => {},
+})
+
+export default authSlice.reducer
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
 
 
 function Login() {
@@ -10,6 +12,10 @@ function Login() {
     })
 
     const { email, password } = formData
+
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
 
     const onChange = (e) => {
         // use input name to change input state
@@ -22,6 +28,13 @@ function Login() {
     // --------------------Sign in user------------------------------/
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData))
     }
     // ----------------------------------------------------------------/
 
@@ -29,7 +42,7 @@ function Login() {
         <>
             <section className="heading">
                 <h1>
-                    <FaSignInAlt />Login
+                    <FaSignInAlt />Login{user}
                 </h1>
                 <p>Please log in to get support</p>
             </section>
