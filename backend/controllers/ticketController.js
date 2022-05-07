447b483559a89822dc113f185d029ca5f28db00c
Lawrence <lawrencesalinas@yahoo.com>
2022-05-07 15:16:35 -0400

    full crud on the back end completed

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler')
 const User = require('../model/userModel')
 const Ticket = require('../model/ticketModel')
 
-//------------------------------------------------------------------------------//
+//---------------------------GET ALL USER TICKETS----------------------------------------//
 // @desc    Get user tickets
 // @route   GET  /app/tickets
 // @access  Private
@@ -22,7 +22,7 @@ const getTickets = asyncHandler(async (req, res) => {
 })
 //------------------------------------------------------------------------------//
 
-//------------------------------------------------------------------------------//
+//-----------------------------CREATE A TICKET----------------------------------//
 //@desc     Create new ticket
 // @route   POST  /app/tickets
 // @access  Private
@@ -53,7 +53,7 @@ const createTicket = asyncHandler(async (req, res) => {
 })
 //------------------------------------------------------------------------------//
 
-//------------------------------------------------------------------------------//
+//------------------------------GET SINGLE TICKET----------------------------------//
 // @desc    Get user tickets
 // @route   GET  /app/tickets
 // @access  Private
@@ -82,9 +82,9 @@ const getTicket = asyncHandler(async (req, res) => {
 })
 //------------------------------------------------------------------------------//
 
-//------------------------------------------------------------------------------//
-// @desc    Get user tickets
-// @route   GET  /app/tickets
+//-------------------------------DELETE TICKET------------------------------------//
+// @desc    Delete tickets
+// @route   DELETE  /app/tickets/:id
 // @access  Private
 const deleteTicket = asyncHandler(async (req, res) => {
   // Get user using  the id in the JWT
@@ -95,9 +95,56 @@ const deleteTicket = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 
-  const tickets = await Ticket.find({ user: req.user.id })
+  const ticket = await Ticket.findById(req.params.id)
 
-  res.status(200).json(tickets)
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  await ticket.remove()
+
+  res.status(200).json({ success: true })
+})
+//------------------------------------------------------------------------------//
+
+//------------------------------UPDATE TICKET----------------------------------//
+// @desc    Update ticket
+// @route   PUT  /app/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user using  the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+
+  res.status(200).json(updatedTicket)
 })
 //------------------------------------------------------------------------------//
 
@@ -105,4 +152,6 @@ module.exports = {
   getTickets,
   createTicket,
   getTicket,
+  deleteTicket,
+  updateTicket,
 }
